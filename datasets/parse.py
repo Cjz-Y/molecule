54a9dd6043855585aafc6f1409f073e61c8e80a8
ironartisan <ironerhalo@gmail.com>
2020-07-17 16:43:57 +0800

    Add methods

--- datasets/parse.py
+++ datasets/parse.py
@@ -1,3 +1,113 @@
 """
 添加对文件进行解析的方法
 """
+import re
+import ase
+
+
+def parse_struc_file(file_name):
+    fe_au_atoms = []
+    iron_idx = get_Fe_atoms(file_name)
+    with open(file_name, 'r') as orig_fd:
+        # very small file
+        orig_str = orig_fd.readlines()
+
+    for line_idx in range(len(orig_str)):
+        if 'Fe' in orig_str[line_idx]:
+            orig_str[line_idx] = orig_str[line_idx].replace('Fe', 'Au', 1)
+            tmp_file_name = 'tmp.struct'
+            with open(tmp_file_name, 'w') as tmp_fd:
+                tmp_fd.writelines(orig_str)
+            at = ase.io.read(tmp_file_name)
+            fe_au_atoms.append(at)
+            orig_str[line_idx] = orig_str[line_idx].replace('Au', 'Fe', 1)
+
+    return iron_idx, fe_au_atoms
+
+
+def parse_scf_file(file_name, iron_idx):
+    mm = get_MM(file_name, iron_idx)
+    hff = get_HFF(file_name, iron_idx)
+    eta = get_ETA(file_name, iron_idx)
+    efg = get_EFG(file_name, iron_idx)
+    rto = get_RTO(file_name, iron_idx)
+    ret = []
+    for i in range(len(iron_idx)):
+        ret.append((mm[i], hff[i], eta[i], efg[i], rto[i]))
+    return ret
+
+
+def get_lines(scffile_path):
+    with open(scffile_path, 'r') as f:
+        text = f.readlines()
+        return text
+
+
+def get_Fe_atoms(structfile_path):
+    Fe_atoms = []
+    for n,line in enumerate(get_lines(structfile_path)):
+        line = [i for i in line.split(' ') if i != '']
+        if line[0] == "ATOM":
+            serial_number = ""
+            for i in line[1]:
+                if i in "0123456789":
+                    serial_number = serial_number + i
+            serial_number = int(serial_number)
+        if "Fe" in line[0]:
+            Fe_atoms.append(serial_number)
+    return Fe_atoms
+
+
+def get_MM(scffile_path, iron_idx):
+    MM = []
+    pat_MM = re.compile(r':MMI\d{3}:.+?=.*')
+    for line in get_lines(scffile_path):
+        match_MM = pat_MM.match(line)
+        if match_MM:
+            match_MM = match_MM.group().split()[-1]
+            MM.append(match_MM)
+    return MM[: len(iron_idx)]
+
+
+def get_HFF(scffile_path, iron_idx):
+    HFF = []
+    pat_HFF = re.compile(r':HFF\d{3}:.*')
+    for line in get_lines(scffile_path):
+        match_HFF = pat_HFF.match(line)
+        if match_HFF:
+            match_HFF = match_HFF.group().split()[-2]
+            HFF.append(match_HFF)
+    return HFF[: len(iron_idx)]
+
+
+def get_ETA(scffile_path, iron_idx):
+    ETA = []
+    pat_ETA = re.compile(r':ETA\d{3}:.*')
+    for line in get_lines(scffile_path):
+        match_ETA = pat_ETA.match(line)
+        if match_ETA:
+            match_ETA = match_ETA.group().split()[-1]
+            ETA.append(match_ETA)
+    return ETA[: len(iron_idx)]
+
+
+def get_EFG(scffile_path, iron_idx):
+    EFG = []
+    pat_EFG = re.compile(r':EFG\d{3}:.+?=.*')
+    for line in get_lines(scffile_path):
+        match_EFG = pat_EFG.match(line)
+        if match_EFG:
+            match_EFG = match_EFG.group().split()[-5]
+            EFG.append(match_EFG)
+    return EFG[: len(iron_idx)]
+
+
+def get_RTO(scffile_path, iron_idx):
+    RTO = []
+    pat_RTO = re.compile(r':RTO\d{3}:.*')
+    for line in get_lines(scffile_path):
+        match_RTO = pat_RTO.match(line)
+        if match_RTO:
+            match_RTO = match_RTO.group().split()[-1]
+            RTO.append(match_RTO)
+    return RTO[: len(iron_idx)]
--- datasets/utils.py
+++ datasets/utils.py
@@ -1,6 +1,14 @@
+import ase
+import os
+from .parse import parse_scf_file,parse_struc_file
+from ase.db import connect
+import logging
 """
 针对数据集的工具类
 """
+
+logger = logging.getLogger(__name__)
+
 
 def generate_ase_db(root_folder, target_path, db_name):
     """
@@ -10,4 +18,41 @@ def generate_ase_db(root_folder, target_path, db_name):
     :param db_name: 生成db名称
     :return:
     """
-    pass
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+    db_path = os.path.join(target_path, db_name)
+
+    if os.path.exists(db_path):
+        os.remove(db_path)
+    db = connect(db_path)
+
+    for root, dirs, files in os.walk(root_folder):
+        if len(files) != 2:
+            logger.info('file count != 2, pass')
+            continue
+        struct_name, scf_name = '', ''
+        for f in files:
+            file_name = os.path.join(root, f)
+            if f.endswith('.struct') and struct_name == '':
+                struct_name = file_name
+            elif f.endswith('.scf') and scf_name == '':
+                scf_name = file_name
+            else:
+                logger.error('file format error.')
+                break
+        else:
+            iron_idx, at_lst = parse_struc_file(struct_name)
+            props_lst = parse_scf_file(scf_name, iron_idx)
+
+            if len(at_lst) != len(props_lst):
+                logger.info('struct file and scf file is not a pair? diff length...')
+                continue
+            else:
+                for i in range(len(at_lst)):
+                    if '' not in props_lst[i]:
+                        db.write(at_lst[i], data={'mm': float(props_lst[i][0]),
+                            'hff': float(props_lst[i][1]),
+                            'eta': float(props_lst[i][2]),
+                            'efg': float(props_lst[i][3]),
+                            'rto': float(props_lst[i][4])
+                            })
